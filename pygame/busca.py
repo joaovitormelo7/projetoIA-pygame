6993ff21cc0609bc9362fe68ed14a5902f6690df
João Vitor Melo <joaovitormelo199@gmail.com>
2024-11-12 12:12:49 -0300

    adicionando pygame e algoritmos de busca

--- pygame/busca.py
+++ pygame/busca.py
@@ -1,2 +1,36 @@
 from collections import deque
 
+#Usando busca em largura
+def buscaLargura(labirinto, start, goal):
+    fila = deque([start])
+    visitado = set()
+    visitado.add(start)
+
+    #Encontrar o objetivo
+    while fila:
+        x, y = fila.popleft()
+        if(x, y) == goal:
+            return True
+        for nx, ny in labirinto.vizinhos_validos(x, y):
+            if(nx, ny) not in visitado:
+                visitado.add((nx,ny))
+                fila.append((nx, ny))
+            return False
+
+#Usando busca em profundidade
+def buscaProfundidade(labirinto, start, goal):
+    pilha = [(start)]
+    visitado = set()
+    while pilha:
+        x, y = pilha.pop()
+        if (x, y) == goal:
+            return True 
+        visitado.add((x, y))
+        for nx, ny in labirinto.vizinhos_validos(x, y):
+            if (nx, ny) not in visitado:
+                pilha.append((nx, ny))
+            return False
+        
+#Usando busca heurística A* com distância Manhattan
+
+        
--- pygame/main.py
+++ pygame/main.py
@@ -1,2 +1,30 @@
 import pygame
-print(pygame.version.ver)
+import sys
+
+#Iniciar pygame
+pygame.init()
+
+#Configuração da janela
+largura, altura = 600, 600
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Visualização do labirinto")
+
+#Cor 
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+
+#Loop principal
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running == False
+    
+    tela.fill(BRANCO)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
