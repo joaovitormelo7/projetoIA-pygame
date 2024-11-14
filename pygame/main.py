cb869ee206b0920247b125217f772e96538a7f47
João Vitor Melo <joaovitormelo199@gmail.com>
2024-11-14 14:28:36 -0300

    corrigindo agente invisivel

--- pygame/main.py
+++ pygame/main.py
@@ -34,6 +34,11 @@ else:
     
 passo_atual = 0
 
+#Animação
+posicao_animada = [posicao_agente[1] * tamanho_celula + tamanho_celula // 2,
+                  posicao_agente[0] * tamanho_celula + tamanho_celula // 2]
+velocidade = 5    
+
 #Labirinto
 def labirinto_desenho():
     for x in range(12):
@@ -41,20 +46,16 @@ def labirinto_desenho():
             cor = BRANCO if labirinto.grid[x][y] == 1 else PRETO
             pygame.draw.rect(tela, cor, (y * tamanho_celula, x * tamanho_celula, tamanho_celula, tamanho_celula))
 
-def agente_desenho(posicao_agente, goal):
-
-    # Desenhar o agente
-    pygame.draw.circle(tela, VERDE, 
-                       (posicao_agente[1] * tamanho_celula + tamanho_celula // 2, 
-                        posicao_agente[0] * tamanho_celula + tamanho_celula // 2), 
-                       tamanho_celula // 3)
+def agente_desenho(goal):
+    # Desenhar o agente na posição animada
+    pygame.draw.circle(tela, VERDE, (int(posicao_animada[0]), int(posicao_animada[1])), tamanho_celula // 3)
     
     # Desenhar o objetivo
     pygame.draw.circle(tela, AZUL, 
                        (goal[1] * tamanho_celula + tamanho_celula // 2, 
                         goal[0] * tamanho_celula + tamanho_celula // 2), 
                        tamanho_celula // 3)
-
+    
 
 #Loop do jogo
 running = True
@@ -66,17 +67,37 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
     if caminho and passo_atual < len(caminho):
-        posicao_agente = caminho[passo_atual]
-        passo_atual += 1
+        proxima_posicao = caminho[passo_atual]
+    
+    if labirinto.grid[proxima_posicao[0]][proxima_posicao[1]] == 1:
+        objetivo_x = proxima_posicao[1] * tamanho_celula + tamanho_celula // 2
+        objetivo_y = proxima_posicao[1] * tamanho_celula + tamanho_celula // 2
+        
+
+        if posicao_animada[0] < objetivo_x:
+            posicao_animada[0] += velocidade
+        elif posicao_animada[0] > objetivo_x:
+            posicao_animada[0] -= velocidade
+
+        if posicao_animada[1] < objetivo_y:
+            posicao_animada[1] += velocidade
+        elif posicao_animada[1] > objetivo_y:
+            posicao_animada[1] -= velocidade
+        
+        if abs(posicao_animada[0] - objetivo_x) < velocidade and abs(posicao_animada[1] - objetivo_y) < velocidade:
+            posicao_animada = [objetivo_x, objetivo_y]
+            posicao_agente = proxima_posicao
+            passo_atual += 1
 
     tela.fill(BRANCO)
     labirinto_desenho()
-    agente_desenho(posicao_agente, goal)
+    agente_desenho(goal)
 
     pygame.display.flip()
-    ControleVelocidade.tick(1)
+    ControleVelocidade.tick(30)
 
-pygame.display.update()
+
 pygame.quit()
 sys.exit()
